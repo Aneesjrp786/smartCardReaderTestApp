a24899ef9ab6bcb9b4270a35c21b4b48daa54f1e
Anees Ahmad <Anees.saufik@gmail.com>
2023-10-18 16:31:18 +0500

    pushed changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SmartCardReader} from "../../../smartcardreader";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  async startConnection() {
+    await SmartCardReader.connect();
+  }
+
+  async disconnect() {
+    await SmartCardReader.disconnect();
+  }
 }
